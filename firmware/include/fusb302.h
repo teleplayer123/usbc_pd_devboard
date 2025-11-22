50074131957e14adf621da1286fc06142086fd02
cole sashkin <csashceh1024@gmail.com>
2025-11-22 14:44:21 -0800

    update

---
+++ firmware/include/fusb302.h
@@ -0,0 +1,28 @@
+
+
+#define FUSB302_ADDR                0x22   // 7-bit address
+
+#define FUSB302_REG_DEVICE_ID       0x01
+#define FUSB302_REG_SWITCHES0       0x02
+#define FUSB302_REG_SWITCHES1       0x03
+#define FUSB302_REG_MEASURE         0x04
+#define FUSB302_REG_SLICE           0x05
+#define FUSB302_REG_CONTROL0        0x06
+#define FUSB302_REG_CONTROL1        0x07
+#define FUSB302_REG_CONTROL2        0x08
+#define FUSB302_REG_CONTROL3        0x09
+#define FUSB302_REG_MASK1           0x0A
+#define FUSB302_REG_POWER           0x0B
+#define FUSB302_REG_RESET           0x0C
+#define FUSB302_REG_OCP             0x0D
+#define FUSB302_REG_MASKA           0x0E
+#define FUSB302_REG_MASKB           0x0F
+#define FUSB302_REG_CONTROL4        0x10
+#define FUSB302_REG_STATUS0A        0x3C
+#define FUSB302_REG_STATUS1A        0x3D
+#define FUSB302_REG_INTERRUPTA      0x3E
+#define FUSB302_REG_INTERRUPTB      0x3F
+#define FUSB302_REG_STATUS0         0x40
+#define FUSB302_REG_STATUS1         0x41
+#define FUSB302_REG_INTERRUPT       0x42
+#define FUSB302_REG_FIFOS           0x43
