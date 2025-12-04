d1e365c83c4c1514fd015f2db4fe4c2db08b5c11
Cole Sashkin <csashceh1024@gmail.com>
2025-12-04 11:38:36 -0800

    update

--- firmware/pd_debug/pd_debug.c
+++ firmware/pd_debug/pd_debug.c
@@ -2,7 +2,6 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/i2c.h>
-#include <libopencm3/cm3/nvic.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
@@ -82,17 +81,17 @@ static void usart_printf(const char *format, ...) {
 }
 
 static void print_byte_as_bits(uint8_t byte, uint8_t reg) {
-    printf("Reg %02X: ", reg);
+    usart_printf("Reg %02X: ", reg);
     for (int i = 7; i >= 0; i--) { // Loop from most significant bit (7) to least significant (0)
         // Use a bitwise AND with a mask to check if the current bit is set
         // The mask is 1 shifted left by 'i' positions
         if ((byte >> i) & 1) { 
-            printf("1");
+            usart_printf("1");
         } else {
-            printf("0");
+            usart_printf("0");
         }
     }
-    printf("\r\n");
+    usart_printf("\r\n");
 }
 
 void dump_bits(uint8_t reg, const struct bit_name *tbl)
@@ -101,7 +100,7 @@ void dump_bits(uint8_t reg, const struct bit_name *tbl)
         if (tbl[i].name == NULL)
             continue;  // skip unused bits             
 
-        printf("%s = %d\n", tbl[i].name, !!(reg & tbl[i].mask));
+        usart_printf("%s = %d\n", tbl[i].name, !!(reg & tbl[i].mask));
     }
 }
 
