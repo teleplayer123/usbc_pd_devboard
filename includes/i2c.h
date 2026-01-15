0317e479fbe538f642aa7ad4486f81993ddaada9
cole sashkin <csashceh1024@gmail.com>
2026-01-14 20:44:45 -0800

    update

--- includes/i2c.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <stdlib.h>
-
-int i2c_xfer(uint32_t i2c, uint8_t addr, uint8_t *out, int out_len, uint8_t *in, int in_len);
-uint8_t i2c_read_reg(uint32_t i2c, uint8_t reg);
-void i2c_write_reg(uint32_t i2c, uint8_t reg, uint8_t val);
-void i2c_read_fifo(uint32_t i2c, uint8_t *data, size_t len);
