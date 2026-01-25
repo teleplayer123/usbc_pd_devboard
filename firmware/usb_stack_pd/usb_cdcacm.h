785526261a069bc857bc1877d79eae53b357086f
cole sashkin <csashceh1024@gmail.com>
2026-01-25 12:44:50 -0800

    update

--- firmware/usb_stack_pd/usb_cdcacm.h
+++
@@ -1,8 +0,0 @@
-#include <libopencm3/usb/usbd.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <stdarg.h>
-
-usbd_device *usb_cdcacm_init(void (*rx_cb)(uint8_t *buf, int len));
-void usb_cdcacm_write(uint8_t *buf, int len);
-int usb_printf(const char *format, ...);
